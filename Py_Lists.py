c026ec0e17a04ce207c902de16b7450d7a372653
Ivrah-aymar <ramyakharvi25@gmail.com>
2025-05-13 20:44:46 +0530

    Create Py_Lists.py
    Lists

---
+++ Py_Lists.py
@@ -0,0 +1,13 @@
+#Lists
+animelist=['Haikyu','One Piece', 'Naruto']
+print(animelist)
+mangalist=['IC','TB','Windbreaker','ORV']
+print(animelist + mangalist)
+mangalist=['IC','One Piece','Windbreaker','ORV']
+print(animelist + mangalist)
+del mangalist[1]
+print(mangalist)
+animelist.append('Spy Family')
+print(animelist)
+print(mangalist * 2)
+print(len(animelist))
