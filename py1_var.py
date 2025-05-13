064b5e36a8364452d55b9faf195921b55a1a8134
Ivrah-aymar <ramyakharvi25@gmail.com>
2025-05-13 16:45:46 +0530

    Create py1_var.py
    Variables

---
+++ py1_var.py
@@ -0,0 +1,32 @@
+#Variables
+age1=30
+print(age1)
+name="Urewa Monkey D Luffy"
+print(name)
+Luffy,Sabo,Ace = 17,19,19
+print(Luffy)
+print(Sabo)
+print(Ace)
+Luffy=Sanji=Zoro=2
+print(Luffy)
+print(Sanji)
+print(Zoro)
+
+age2=4
+print(age1+age2)
+print(age1-age2)
+print(age1*age2)
+print(age1/age2)
+print(age1%age2)
+
+#strings
+str1="Nami"
+str2="Swan"
+print(str1+" "+str2)
+#print(str1-str2)
+#TypeError: unsupported operand type(s) for -: 'str' and 'str'
+print(str2*4)
+#index
+print(str1[0:3])
+#prints Nam
+
