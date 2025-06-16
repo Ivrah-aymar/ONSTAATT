57909fc22a15db972bf489a8b5682946bf35ac49
Ivrah-aymar <ramyakharvi25@gmail.com>
2025-06-16 22:42:32 +0530

    Create PY_patterncent.py

---
+++ Learn_Python/PY_patterncent.py
@@ -0,0 +1,16 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+n,m=map(int,input().split(" "))
+
+s="WELCOME"
+#print(m)
+#print(n)
+#print(s)
+for i in range(1, n, 2):
+    pattern = '.|.' * i
+    print(pattern.center(m, '-'))
+
+print('WELCOME'.center(m, '-'))
+
+for i in range(n-2, 0, -2):
+    pattern = '.|.' * i
+    print(pattern.center(m, '-'))
